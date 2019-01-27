70ee78ac43cd0a64edc2923bf1bef4b23622bad1
Victor Mimo <victormimo@mys-MacBook.local>
2019-01-27 18:44:05 -0500

    Added home component installed react-router-dom added temp logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import logo from "./logo-temp.png";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} alt="logo" />
+            <p>What are you hacking tonight?</p>
+            <Listings />
+            <Route path="/" component={Home} />
+            <Route path="/lead" component={Teacher} />
+            <Route path="/hack" component={Student} />
+          </header>
+        </div>
+      </Router>
     );
   }
 }
+
+const Listings = () => {
+  return (
+    <Fragment>
+      <Link to="/hack" className="App-link">
+        I want to learn
+      </Link>
+      <Link to="/lead" className="App-link">
+        I want to teach
+      </Link>
+    </Fragment>
+  );
+};
+
+const Home = () => <div />;
+
+const Teacher = () => <h2>hello hacker master</h2>;
+
+const Student = () => <h2>hello hacker</h2>;
 
 export default App;
